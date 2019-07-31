b099f1d75d8def709fde6199ca91675cb722d8f4
Jordan <Jordan@AKEANE-PC>
2019-07-31 12:55:44 +0100

    Add project files.

---
+++ ToDoListJD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToDoListJD.MvcApplication" Language="C#" %>
---
+++ ToDoListJD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToDoListJD.Startup))]
+namespace ToDoListJD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
